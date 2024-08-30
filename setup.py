f3ab490b0e669ee0e6bf07765ec49a951b093f27
Xiao Jun Jiang <william.jiang@xperi.com>
2024-08-30 08:03:07 -0700

    I keep it as it is, and merge into playground-python.

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages, setup
 
+with open("README.md", mode="r", encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
 setup(
     name="my_flask_app",
     version="0.1.0",
     author="ML Support Team",
     description="Python Flask App",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     url="https://artifacts.corp.xperi.com:443/artifactory/xperi-ml-pypi/my_flask_app/0.1.0/",
     packages=find_packages(),
     include_package_data=True,
